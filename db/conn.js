8222b1b19e8db069d592d31e47df456565f6744f
alaanvv <valealan84@gmail.com>
2023-03-18 13:24:23 -0300

    added a lot of commands

--- db/conn.js
+++ db/conn.js
@@ -14,4 +14,22 @@ conn.cmd = async query => {
   })
 }
 
-module.exports = conn
+// Utils
+async function getAll(table, where) {
+  const res = await conn.cmd(`select * from ${table} where ${where}`)
+  return res
+}
+async function get(table, where) { 
+  const res = await getAll(table, where)
+  return res[0]
+}
+
+async function set(table, values) {
+  await conn.cmd(`insert into ${table} values (${values})`)
+}
+async function update(table, column, value, where) {
+  if (typeof value === String) value = `'${value}'`
+  await conn.cmd(`update ${table} set ${column} = ${value} where ${where}`)
+}
+
+module.exports = { conn, get, getAll, set, update }
